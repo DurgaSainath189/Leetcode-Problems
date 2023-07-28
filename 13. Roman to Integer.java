26696e097d4cf3dc9e96a4d2baebed50dd1a0d2c
Durga Sainath Dora <durgasainathdora@gmail.com>
2023-07-28 17:40:48 +0530

    Roman to Integer

---
+++ 13. Roman to Integer.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int romanToInt(String s) {
+        int result = 0;
+        int sum = 0;
+        for (int i = s.length()-1; i>=0; i--) {
+            char letter = s.charAt(i);
+            if (letter == 'I') result = 1;
+            if (letter == 'V') result = 5;
+            if (letter == 'X') result = 10;
+            if (letter == 'L') result = 50;
+            if (letter == 'C') result = 100;
+            if (letter == 'D') result = 500;
+            if (letter == 'M') result = 1000;
+            if(4*result<sum){
+                sum=sum-result;
+            }
+            else{
+                sum+=result;
+            }
+        }
+        return sum;
+    }
+}
